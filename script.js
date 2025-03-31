3b167288e9e6c8305d9092909d4557c8094e18c4
arun12421426 <onlyformoviesarun@gmail.com>
2025-03-31 21:45:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('inquiry-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your inquiry! We will get back to you soon.');
+});
